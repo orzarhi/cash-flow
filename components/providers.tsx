9c91d1d42d01dfa9a3311ff7d8e983b16d1de68e
orzarhi <or.zarhi11@gmail.com>
2024-10-26 23:17:38 +0300

    refactor: Remove react-pull-to-refresh dependency and update Providers component
    This commit removes the react-pull-to-refresh dependency from the package.json file and updates the Providers component to remove the PullToRefresh component. The useScrollReload hook is no longer used and the handleRefresh function has been removed. This change simplifies the code and improves performance by eliminating unnecessary dependencies and code execution.

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,7 +2,6 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import PullToRefresh from 'react-pull-to-refresh';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -11,20 +10,11 @@ interface ProvidersProps {
 const queryClient = new QueryClient();
 
 export const Providers = ({ children }: ProvidersProps) => {
-  const handleRefresh = () => {
-    return new Promise<void>((resolve) => {
-      window.location.reload();
-      resolve();
-    });
-  };
-
   return (
-    <PullToRefresh onRefresh={handleRefresh}>
-      <QueryClientProvider client={queryClient}>
-        <NextThemesProvider attribute="class" enableSystem={true} defaultTheme="system">
-          {children}
-        </NextThemesProvider>
-      </QueryClientProvider>
-    </PullToRefresh>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider attribute="class" enableSystem={true} defaultTheme="system">
+        {children}
+      </NextThemesProvider>
+    </QueryClientProvider>
   );
 };
